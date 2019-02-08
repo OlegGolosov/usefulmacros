8bc25d07830f36a857e0dbbb3e5cab97de5b39b2
Oleg Golosov <oleg.golosov.com>
2019-02-09 00:53:37 +0300

    1D restyle

--- COMPAREROOT.py
+++ COMPAREROOT.py
@@ -4,8 +4,6 @@ import sys
 
 import argparse
 import ROOT
-from ROOT import gPad
-
 
 parser = argparse.ArgumentParser(description='Compare root files')
 
@@ -47,6 +45,7 @@ keys = list(set(keys_1).intersection(keys_2))
 histograms = [(name, subdir_1.Get(name), subdir_2.Get(name)) for name in sorted(keys) ]
 
 c = ROOT.TCanvas('c_first', 'c_first')
+
 text = ROOT.TLatex()
 text.SetNDC()
 text.SetTextSize(0.035)
@@ -54,7 +53,7 @@ text.DrawLatex(0.1, 0.8, 'Comparing ')
 text.DrawLatex(0.1, 0.7, file_1_path)
 text.DrawLatex(0.1, 0.6, 'and')
 text.DrawLatex(0.1, 0.5, file_2_path)
-c.Print(output_path + '.pdf(','pdf')
+c.Print(output_path + '.pdf(','Title:Start')
 
 
 for hist_name, hist_1, hist_2 in histograms:
@@ -68,46 +67,59 @@ for hist_name, hist_1, hist_2 in histograms:
         scale_factor = 1.0*hist_1.GetEntries()/hist_2.GetEntries() if hist_2.GetEntries() != 0 else 1
         hist_2.Scale(scale_factor)
     
-    c = ROOT.TCanvas('c_' + hist_name, hist_1.GetTitle())
+    title = hist_1.GetTitle()
+    c = ROOT.TCanvas('c_' + hist_name, title)
+    ROOT.gStyle.SetOptTitle(0)
     c.UseCurrentStyle()
-
     if args.labels is not None:
         hist_1.SetTitle(args.labels[0])
         hist_2.SetTitle(args.labels[1])
-
     c.cd()
     if type(hist_1).__name__.startswith("TH1") and type(hist_2).__name__.startswith("TH1"):
-        data_maximum = hist_1.GetBinContent(hist_1.GetMaximumBin())
-        mc_maximum = hist_2.GetBinContent(hist_2.GetMaximumBin())
-
-        hist_1.SetLineColor(ROOT.kRed)
+        stack = ROOT.THStack ('st_' + hist_name, title)
         hist_1.SetLineWidth(2)
-        hist_2.SetLineColor(ROOT.kBlack)
+        hist_1.SetLineColor(ROOT.kRed)
+        hist_1.Draw()
+        stat_1 = hist_1.GetListOfFunctions().FindObject('stats')
+        stat_1.SetTextColor(ROOT.kRed)
+        stat_1.SetX1NDC(0.8)
+        stat_1.SetX2NDC(1.0)
+        stat_1.SetY1NDC(0.84)
+        stat_1.SetY2NDC(1.0)
+        statTitle = stat_1.GetLineWith(hist_1.GetName())
+        statTitle.SetText(0,0,hist_1.GetTitle())
         hist_2.SetLineWidth(2)
-
-        if data_maximum > mc_maximum:
-            hist_1.Draw("l hist")
-            c.Update()
-            hist_2.Draw("l hist same")
-        else:
-            hist_2.Draw("l hist")
-            c.Update()
-            hist_1.Draw("l hist same")
-        ROOT.gPad.BuildLegend()
+        hist_2.SetLineColor(ROOT.kBlack)
+        hist_2.Draw()
+        stat_2 = hist_2.GetListOfFunctions().FindObject('stats')
+        stat_2.SetTextColor(ROOT.kBlack)
+        stat_2.SetX1NDC(0.8)
+        stat_2.SetX2NDC(1.0)
+        stat_2.SetY1NDC(0.68)
+        stat_2.SetY2NDC(0.84)
+        statTitle = stat_2.GetLineWith(hist_2.GetName())
+        statTitle.SetText(0,0,hist_2.GetTitle())
+        ROOT.gPad.SetRightMargin(0.2)
+        
+        stack.Add(hist_1)
+        stack.Add(hist_2)
+        stack.Draw("nostack")
+      
+        ROOT.gPad.BuildLegend(0.8,0.0,1.0,0.68)
+        
     elif type(hist_1).__name__.startswith("TH2") and type(hist_2).__name__.startswith("TH2"):
         c.Divide(2,1)
         c.cd(1)
-        gPad.SetLogz()
+        ROOT.gPad.SetLogz()
         hist_1.Draw("colz")
         c.cd(2)
-        gPad.SetLogz()
+        ROOT.gPad.SetLogz()
         hist_2.Draw("colz")
     
-    c.Update()
+    c.Print(output_path + '.pdf','Title:'+ title)
     c.Write()
-    c.Print(output_path + '.pdf','pdf')
 
 c = ROOT.TCanvas('c_last', 'The end!')
 text.DrawLatex(0.5, 0.6, 'The end! ')
-c.Print(output_path + '.pdf)','pdf')
+c.Print(output_path + '.pdf)','Title:Finish')
 
