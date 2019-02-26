ee05b5a5789ad38a9e399ee45305004906e3eb0a
Oleg Golosov <oleg.golosov.com>
2019-02-26 16:19:39 +0300

    variable number of files

--- compareRootFiles.py
+++ compareRootFiles.py
@@ -4,12 +4,13 @@ import sys
 
 import argparse
 import ROOT
+import math
 
 parser = argparse.ArgumentParser(description='Compare root files')
 
-parser.add_argument('-i', '--input', dest='input', help='Input root files', nargs=2, required=True)
-parser.add_argument('-l', '--labels', dest='labels', help='Labels for each file', nargs=2, required=False)
-parser.add_argument('-o', '--output', dest='output', help='Output file', required=True)
+parser.add_argument('-i', '--input', dest='input', help='Input root files', nargs='+', required=True)
+parser.add_argument('-l', '--labels', dest='labels', help='Labels for each file', nargs='+', required=False)
+parser.add_argument('-o', '--output', dest='output', help='Output file', default='comp.root')
 parser.add_argument('-d', '--directory', dest='directory', help='Directory to compare', required=False)
 parser.add_argument('--rescale', dest='rescale', help='Rescale histograms', required=False, type=bool, default=False)
 
@@ -19,40 +20,51 @@ print(args)
 ROOT.gROOT.SetBatch(True)
 ROOT.gStyle.SetOptStat(111111)
 ROOT.gStyle.SetTitleAlign(33)
+colors = [ROOT.kBlack, ROOT.kRed, ROOT.kBlue, ROOT.kGreen+2, ROOT.kMagenta+2, ROOT.kOrange+2, ROOT.kPink+2, ROOT.kTeal+2, ROOT.kCyan+2, ROOT.kCyan+4, ROOT.kAzure, ROOT.kGray+3, ROOT.kOrange+7, ROOT.kGreen+4]
 
-file_1_path = args.input[0]
-file_2_path = args.input[1]
 output_path = args.output
 
-dir_1 = ROOT.TFile.Open(file_1_path)
-dir_2 = ROOT.TFile.Open(file_2_path)
+dirs = []
+for file_path in args.input:
+  dirs.append(ROOT.TFile.Open(file_path))
+  if args.directory is not None:
+    dir=dir.Get(args.directory)
 
-if args.directory is not None:
-    subdir_1 = dir_1.Get(args.directory)
-    subdir_2 = dir_2.Get(args.directory)
-else:
-    subdir_1 = dir_1
-    subdir_2 = dir_2
-
+i = 0
 labels = []
 if args.labels is not None:
-    labels.append(args.labels[0])
-    labels.append(args.labels[1])
-else:
-    labels.append(file_1_path.rsplit('/', 1)[1])
-    labels.append(file_2_path.rsplit('/', 1)[1])
-    labels[0] = labels[0].rsplit('.root', 1)[0]
-    labels[1] = labels[1].rsplit('.root', 1)[0]
+  for label in args.labels:
+    labels.append(label)
+  if len(args.labels) < len(args.input):
+    print 'Warning! Number of labels provided is less than number of input files!'
+    i = len(labels)
+
+while i < len (args.input):
+  label = args.input[i]
+  if '/' in label: 
+    label = label.rsplit('/', 1)[-1]
+  if label.endswith('.root'):
+    label = label.rsplit('.root', 1)[0]
+  labels.append(label)
+  i+=1
   
 output_file = ROOT.TFile.Open(output_path,"recreate")
 output_path_pdf=output_path.replace(".root",".pdf") 
 
+object_names = []
+for key in dirs[0].GetListOfKeys():
+  object_name=key.GetName()
+  hist=dirs[0].Get(object_name)
+  if hist.InheritsFrom("TH1") or hist.InheritsFrom("TGraph") or hist.InheritsFrom("TMultiGraph"):
+    object_names.append(object_name)
+  else:
+    print 'Skipping non-histogram object', object_name
 
-keys_1 = [key.GetName() for key in subdir_1.GetListOfKeys()]
-keys_2 = [key.GetName() for key in subdir_2.GetListOfKeys()]
-keys = list(set(keys_1).intersection(keys_2))
-
-histograms = [(name, subdir_1.Get(name), subdir_2.Get(name)) for name in sorted(keys) ]
+for dir in dirs:
+  object_names_temp = []  
+  for key in dir.GetListOfKeys():
+    object_names_temp.append(key.GetName())
+  object_names = list(set(object_names).intersection(object_names_temp))
 
 c = ROOT.TCanvas('c_first', 'c_first')
 
@@ -60,138 +72,123 @@ text = ROOT.TLatex()
 text.SetNDC()
 text.SetTextSize(0.055)
 text.SetTextFont(42)
-text.DrawLatex(0.1, 0.8, 'Comparing ')
-text.DrawLatex(0.1, 0.7, file_1_path + ' (' + labels[0] + ')')
-text.DrawLatex(0.1, 0.6, 'and')
-text.DrawLatex(0.1, 0.5, file_2_path + ' (' + labels[1] + ')')
+text.DrawLatex(0.1, 0.9, 'Comparing ')
+i = 0
+for label in labels:
+  ypos = 0.85 - 0.05 * i
+  text.DrawLatex(0.1, ypos, args.input [i] + ' (' + label + ')')
+  i += 1
 c.Print(output_path_pdf + '(','Title:Title')
     
-
-for hist_name, hist_1, hist_2 in histograms:
-    print(hist_name)
-
-    if not(type(hist_1).__name__.startswith("TH") and type(hist_2).__name__.startswith("TH")):
-        print('Skipping non-histogram object', hist_name)
-        continue
-
-    if args.rescale:
-        scale_factor = 1.0*hist_1.GetEntries()/hist_2.GetEntries() if hist_2.GetEntries() != 0 else 1
-        hist_2.Scale(scale_factor)
-    
-    title = hist_1.GetName()
-    title = title.replace('tex','te')
-    c = ROOT.TCanvas('c_' + hist_name, title)
-    hist_1.SetTitle(labels[0])
-    hist_2.SetTitle(labels[1])
-        
-    if type(hist_1).__name__.startswith("TH1") and type(hist_2).__name__.startswith("TH1"):
-        ROOT.gStyle.SetOptTitle(1)
-        c.cd()
-        stack = ROOT.THStack ('st_' + hist_name, title)
-        hist_1.SetLineWidth(2)
-        hist_1.SetLineColor(ROOT.kRed)
-        hist_1.Draw()
-        ROOT.gPad.Modified()
-        ROOT.gPad.Update()
-        stat_1 = hist_1.GetListOfFunctions().FindObject('stats')
-        stat_1.SetTextColor(ROOT.kRed)
-        stat_1.SetX1NDC(0.8)
-        stat_1.SetX2NDC(1.0)
-        stat_1.SetY1NDC(0.84)
-        stat_1.SetY2NDC(1.0)
-        statTitle = stat_1.GetLineWith(hist_1.GetName())
-        statTitle.SetText(0,0,hist_1.GetTitle())
-        hist_2.SetLineWidth(2)
-        hist_2.SetLineColor(ROOT.kBlack)
-        hist_2.Draw()
-        ROOT.gPad.Modified()
-        ROOT.gPad.Update()
-        stat_2 = hist_2.GetListOfFunctions().FindObject('stats')
-        stat_2.SetTextColor(ROOT.kBlack)
-        stat_2.SetX1NDC(0.8)
-        stat_2.SetX2NDC(1.0)
-        stat_2.SetY1NDC(0.68)
-        stat_2.SetY2NDC(0.84)
-        statTitle = stat_2.GetLineWith(hist_2.GetName())
-        statTitle.SetText(0,0,hist_2.GetTitle())
-        ROOT.gPad.SetRightMargin(0.2)
-        
-        stack.Add(hist_1)
-        stack.Add(hist_2)
-        stack.Draw("nostack")
+for object_name in object_names:
+  print(object_name)
+  
+  ref_hist=dirs[0].Get(object_name).Clone()
+  ref_hist.Sumw2()
+  title = ref_hist.GetName()
+  c = ROOT.TCanvas('c_' + title, title)
+  ROOT.gStyle.SetOptTitle(1)
+  c.cd()
+  
+  if (ref_hist.InheritsFrom("TH2")):
+    ROOT.gStyle.SetOptTitle(0)
+    text.DrawLatex(0.1, 0.95, title)
+    c1 = ROOT.TPad("c1", "c1", 0., 0., 1., 0.95);
+    c1.Draw();
+    c1.cd();
+    npads = len(dirs)
+    npadsx = int(math.ceil (math.sqrt (npads)))
+    npadsy = int(math.ceil (1. * npads / npadsx))
+    c1.Divide(npadsx, npadsy)
+    i = 0
+    for dir in dirs:
+      c1.cd(i+1)
+      ROOT.gPad.SetLogz()
+      ROOT.gPad.SetLeftMargin(0.15)
+      ROOT.gPad.SetTopMargin(0.01)
       
-        ROOT.gPad.BuildLegend(0.8,0.0,1.0,0.68)
-        
-        c.Print(output_path_pdf,'Title:'+ title)
-        c.Write()
-        
-    elif type(hist_1).__name__.startswith("TH2") and type(hist_2).__name__.startswith("TH2"):
-        ratio = hist_1.Clone()
-        ROOT.gStyle.SetOptTitle(0)
-        text.DrawLatex(0.1, 0.95, title)
-        c1 = ROOT.TPad("c1", "c1", 0., 0., 1., 0.95);
-        c1.Draw();
-        c1.cd();
-        c1.Divide(2,1)
-        c1.cd(1)
-        ROOT.gPad.SetLogz()
-        ROOT.gPad.SetLeftMargin(0.15)
-        ROOT.gPad.SetTopMargin(0.01)
-        hist_1.Draw("colz")
-        ROOT.gPad.Modified()
-        ROOT.gPad.Update()
-        stat_1 = ROOT.gPad.GetPrimitive('stats')
-        stat_1.SetName('stat_1')
-        stat_1.SetTextColor(ROOT.kRed)
-        stat_1.SetX1NDC(.7)
-        stat_1.SetX2NDC(.9)
-        stat_1.SetY1NDC(.7)
-        stat_1.SetY2NDC(.99)
-        statTitle = stat_1.GetLineWith(hist_1.GetName())
-        statTitle.SetText(0,0,hist_1.GetTitle())
-        hist_1.SetStats(0)
-        c1.cd(2)
-        ROOT.gPad.SetLogz()
-        ROOT.gPad.SetLeftMargin(0.15)
-        ROOT.gPad.SetTopMargin(0.01)
-        hist_2.Draw("colz")
-        ROOT.gPad.Modified()
-        ROOT.gPad.Update()
-        stat_2 = ROOT.gPad.GetPrimitive('stats')
-        stat_2.SetName('stat_2')
-        stat_2.SetTextColor(ROOT.kBlack)
-        stat_2.SetX1NDC(.7)
-        stat_2.SetX2NDC(.9)
-        stat_2.SetY1NDC(.7)
-        stat_2.SetY2NDC(.99)
-        statTitle = stat_2.GetLineWith(hist_2.GetName())
-        statTitle.SetText(0,0,hist_2.GetTitle())
-        hist_2.SetStats(0)
-        c.Print(output_path_pdf,'Title:'+ title)
-        c.Write()
-    
-        c = ROOT.TCanvas('c_' + hist_name + "_ratio", title)
-        ROOT.gStyle.SetOptTitle(1)
-        #ROOT.gPad.SetLogz()
-        hist_1.Sumw2()
-        hist_2.Sumw2()
-        ratio.Divide (hist_1, hist_2)
-        ROOT.gPad.SetLeftMargin(0.12)
-        ROOT.gPad.SetTopMargin(0.12)
-        ratio.SetName(title)
-        ratio.SetTitle(title + ' ratio ( #frac{' + hist_1.GetTitle() + '}{' + hist_2.GetTitle() + '} )')
-        ratio.Draw("colz")
-        ROOT.gPad.Modified()
-        ROOT.gPad.Update()        
-        stat = ROOT.gPad.GetPrimitive('stats')
-        stat.SetName('stat')
-        stat.SetX1NDC(.7)
-        stat.SetX2NDC(.9)
-        stat.SetY1NDC(.7)
-        stat.SetY2NDC(.99)
-        c.Print(output_path_pdf,'Title:'+ title + "_ratio")
-        c.Write()
+      hist=dir.Get(object_name)
+      hist.SetTitle(labels[i])
+      hist.Draw("colz")
+      ROOT.gPad.Modified()
+      ROOT.gPad.Update()
+      stat = ROOT.gPad.GetPrimitive('stats')
+      stat.SetTextColor(colors [i])
+      stat.SetName('stat')
+      stat.SetX1NDC(.7)
+      stat.SetX2NDC(.9)
+      stat.SetY1NDC(.7)
+      stat.SetY2NDC(.99)
+      statTitle = stat.GetLineWith(hist.GetName())
+      statTitle.SetText(0,0,hist.GetTitle())
+      hist.SetStats(0)
+      i += 1
+      
+    c.Print(output_path_pdf,'Title:'+ title.replace('tex','te'))
+    c.Write()
 
+    c.SetName(c.GetName() + "_ratio")
+    c.cd()
+    text.DrawLatex(0.1, 0.95, title + ": ratio to " + labels [0])
+    i = 0
+    for dir in dirs:
+      hist=dir.Get(object_name)
+      hist.Sumw2()
+      hist.Divide(ref_hist)
+      c1.cd(i+1)
+      ROOT.gPad.SetLogz(0)
+      hist.Draw("colz")
+      i += 1
+    c.Print(output_path_pdf,'Title:'+ title.replace('tex','te') + "_ratio")
+    c.Write()
+
+  elif (ref_hist.InheritsFrom("TH1")):
+    stack = ROOT.THStack ('st_' + object_name, title)
+    i = 0
+    for dir in dirs:
+      hist=dir.Get(object_name)
+      hist.SetTitle(labels[i])
+          
+      if args.rescale:
+        scale_factor = 1.0*hist.GetEntries()/ref_hist.GetEntries() if ref_hist.GetEntries() != 0 else 1
+        hist.Scale(scale_factor)
+      hist.SetLineWidth(2)
+      hist.SetLineColor(colors[i])
+      hist.Draw()
+      ROOT.gPad.Modified()
+      ROOT.gPad.Update()
+      stat=hist.GetListOfFunctions().FindObject('stats')
+      stat.SetTextColor(colors[i])
+      stat.SetX1NDC(0.8)
+      stat.SetX2NDC(1.0)
+      y_offset=0.1*i
+      stat.SetY1NDC(0.9-y_offset)
+      stat.SetY2NDC(1.0-y_offset)
+      statTitle = stat.GetLineWith(hist.GetName())
+      statTitle.SetText(0,0,hist.GetTitle())
+      stack.Add(hist)
+      i+=1
+            
+    ROOT.gPad.SetRightMargin(0.2)
+    stack.Draw("nostack")
+    #ROOT.gPad.BuildLegend(0.8,0.0,1.0,0.9-y_offset)
+    c.Print(output_path_pdf,'Title:'+ title.replace('tex','te'))
+    c.Write()
+    
+    stack = ROOT.THStack ('st_' + object_name + "_ratio", title +  ": ratio to " + labels [0])
+    c.SetName(c.GetName() + "_ratio")
+    i = 0
+    for dir in dirs:
+      hist=dir.Get(object_name)
+      hist.Sumw2()
+      hist.Divide(ref_hist)
+      stack.Add(hist)
+      i += 1
+    stack.Draw("nostack hist")
+    #ROOT.gPad.BuildLegend(0.8,0.0,1.0,0.9-y_offset)
+    c.Print(output_path_pdf,'Title:'+ title.replace('tex','te') + "_ratio")
+    c.Write()
+        
 c = ROOT.TCanvas('c_last', 'The end!')
 text.DrawLatex(0.5, 0.6, 'The end! ')
 
