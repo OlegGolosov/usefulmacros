2fc88c2cfa1d681f8fdeae897017f1ce8f63db49
Oleg Golosov <oleg.golosov@gmail.com>
2019-12-14 16:04:13 +0100

    add beam rapidity

--- PlabToCM.C
+++ PlabToCM.C
@@ -1,15 +1,17 @@
 void PlabToCM(float pl)
 {
 // code to calculate T0 for a given energy
-  Double_t mp=0.938;
-  Double_t El=sqrt(pl*pl+mp*mp);
-  Double_t Ekin=El-mp;
-  Double_t pc=sqrt( ( mp*El-mp*mp )/2 );
-  Double_t e_cm=sqrt( pc*pc + mp*mp );
-  Double_t ss = sqrt( 4*(pc*pc + mp*mp) );
+  float mp=0.938;
+  float El=sqrt(pl*pl+mp*mp);
+  float Ekin=El-mp;
+  float pc=sqrt( ( mp*El-mp*mp )/2 );
+  float e_cm=sqrt( pc*pc + mp*mp );
+  float ss = sqrt( 4*(pc*pc + mp*mp) );
+  float yBeam = 0.25 * log ((El + pl) / (El - pl));
   cout << "E = " << El << endl;
   cout << "Ekin = " << Ekin << endl;
   cout << "P* = " << pc << endl;
   cout << "E* = " << e_cm << endl;
   cout << "sqrt(Snn) = " << ss << endl;
+  cout << "yBeam = " << yBeam << endl;
 }
